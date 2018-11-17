c1c09c4ba47c8939ea6b079fc2123be13f0decf1
sankethande <sanket@perials.com>
2018-11-17 12:59:55 +0530

    Updated list api, Added count api

--- controllers/battle.controller.js
+++ controllers/battle.controller.js
@@ -1,12 +1,43 @@
 const { BattleModel } = require("../db");
 
-function getAll(req, res) {
-    console.log(req);
-    console.log(res);
-    testMongoDb();
-    res.send({data: "TODO"});
+async function getAll(req, res) {
+    // if skip in query string use it else default to 0
+    const skip = +req.query.skip ? +req.query.skip : 0;
+
+    // if limit in query string then use it else default to 10
+    const limit = +req.query.limit ? +req.query.limit : 10;
+
+    // fetch from database
+    const allBattles = await BattleModel
+        .find({})
+        .skip(skip)
+        .limit(limit)
+        .exec();
+    
+    // send response
+    res.send({
+        data: {
+            battles: allBattles
+        }
+    });
 }
 
+async function getCount(req, res) {
+    // fetch count from database
+    const count = await BattleModel.count({}).exec();
+    
+    // send response
+    res.send({
+        data: {
+            count
+        }
+    });
+}
+
+/**
+ * Function for testing MongoDb write permission
+ */
+/*
 function testMongoDb() {
     const instance = new BattleModel();
     instance.year = 1906;
@@ -15,7 +46,9 @@ function testMongoDb() {
         console.log(err); 
     });
 }
+*/
 
 module.exports = {
-    getAll
+    getAll,
+    getCount
 };
--- routes/battle.route.js
+++ routes/battle.route.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const BattleController = require("../controllers/battle.controller");
 
 router.get("/list", BattleController.getAll);
+router.get("/count", BattleController.getCount);
 
 module.exports = router;
